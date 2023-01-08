434c5106ff588d998e40c6dda5604111586e4cbb
Ángel González <aangel.gg@hotmail.com>
2023-01-08 15:30:41 +0100

    Created project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "#F90068",
+      },
+    },
+  },
+  plugins: [],
+};
